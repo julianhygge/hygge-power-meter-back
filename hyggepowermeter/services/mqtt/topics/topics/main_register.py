fbcac53c8c0b010fc259b2f05fffc5c9bcbde866
julianhygge <julian@hygge.energy>
2023-03-29 14:26:57 -0400

    Structure base

--- hyggepowermeter/services/mqtt/topics/topics/main_register.py
+++ hyggepowermeter/services/mqtt/topics/topics/main_register.py
@@ -1,6 +1,6 @@
 import json
 from types import SimpleNamespace
-
+from hyggepowermeter.data.power_meter_repository import power_meter_repository
 from hyggepowermeter.services.mqtt.topics.topic_base import TopicBase
 
 
@@ -8,4 +8,5 @@ class MainRegisters(TopicBase):
     def do_action(self, msg,  config):
         power_meter_reading = json.loads(msg.payload, object_hook=lambda d: SimpleNamespace(**d))
         print(f'This is power meter reading, {msg.payload}')
+        test = power_meter_repository
         # db_client.update_otp_status(booking.resource.bookingId, booking.resource.status)
