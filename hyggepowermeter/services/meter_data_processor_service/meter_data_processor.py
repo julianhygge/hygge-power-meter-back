54ec421807107d7848684e9dd4bfcc43fcd48eaa
julianhygge <julian@hygge.energy>
2023-03-29 15:40:25 -0400

    meter data processor, fix peewee class

--- hyggepowermeter/services/meter_data_processor_service/meter_data_processor.py
+++ hyggepowermeter/services/meter_data_processor_service/meter_data_processor.py
@@ -2,26 +2,23 @@ import time
 from datetime import datetime, timedelta
 import pytz
 from dateutil import tz
-
 from hyggepowermeter.data.power_meter_repository import power_meter_repository
 
 
 class MeterDataProcessorService:
     def __init__(self):
-        pass
+        self.run_hourly_and_daily()
 
     @staticmethod
     def store_hourly_kwh():
-
         # Calculate the start and end times for the past hour
         now = datetime.now()
         local_tz = tz.tzlocal()
         now_utc = now.replace(tzinfo=local_tz).astimezone(pytz.utc)
         end_time = now_utc.replace(minute=0, second=0, microsecond=0)
         start_time = end_time - timedelta(hours=1)
-
         # Get the readings for the past hour
-        readings = power_meter_repository.read_power_meter_readings
+        readings = power_meter_repository.read_power_meter_readings(between=(start_time, end_time))
 
         # Calculate the hourly kWh
         kwh = 0
@@ -35,29 +32,28 @@ class MeterDataProcessorService:
             power = voltage * current
             kwh += (power / 1000) * (average_time_interval / 3600)  # Assuming power is in W
 
-        # # Store the hourly kWh in the new table
-        # cursor.execute(f"INSERT INTO {table_name} (device_id, timestamp, kwh) VALUES (?, ?, ?)",
-        #                (readings[0][1], end_time, kwh))
-        # conn.commit()
-        # conn.close()
-        #
-        # print(f"Hourly kWh stored for {start_time} - {end_time}: {kwh}")
+        data = {
+            "timestamp": end_time,
+            "device_id": 111,
+            "box_id": "box_1",
+            "power": kwh
+        }
 
-    def run_hourly_and_daily(hourly_func, daily_func, db_name, hourly_table_name, daily_table_name):
+        power_meter_repository.insert_hourly_kwh(data)
+
+    def run_hourly_and_daily(self):
         while True:
             now = datetime.now()
             next_hour = now.replace(minute=0, second=0, microsecond=0) + timedelta(hours=1)
             sleep_time = (next_hour - now).total_seconds()
             time.sleep(sleep_time)
-            hourly_func(db_name, hourly_table_name)
+            self.store_hourly_kwh()
             # Check if it's time to store the daily kWh
             if now.hour == 23:
-                daily_func(db_name, hourly_table_name, daily_table_name)
+                self.store_daily_kwh()
 
-    def store_daily_kwh(db_name, hourly_table_name, daily_table_name):
-        db = db_name + ".db"
-        conn = sqlite3.connect(db)
-
+    @staticmethod
+    def store_daily_kwh():
         # Calculate the start and end times for the past day
         now = datetime.now()
         local_tz = tz.tzlocal()
@@ -65,43 +61,8 @@ class MeterDataProcessorService:
         end_date = now_utc.replace(hour=0, minute=0, second=0, microsecond=0)
         start_date = end_date - timedelta(days=1)
 
-        # Get the hourly kWh for the past day
-        cursor = conn.cursor()
-        cursor.execute(f"SELECT * FROM {hourly_table_name} WHERE timestamp >= ? AND timestamp < ?",
-                       (start_date, end_date))
-        hourly_kwh_records = cursor.fetchall()
-
+        # Get the readings for the past hour
+        readings = power_meter_repository.read_power_meter_readings(between=(start_date, end_date))
         # Calculate the daily kWh
-        daily_kwh = sum([record[3] for record in hourly_kwh_records])
-
-        # Store the daily kWh in the new table
-        cursor.execute(f"INSERT INTO {daily_table_name} (device_id, date, kwh) VALUES (?, ?, ?)",
-                       (hourly_kwh_records[0][1], end_date.date(), daily_kwh))
-        conn.commit()
-        conn.close()
-
-        print(f"Daily kWh stored for {start_date.date()} - {end_date.date()}: {daily_kwh}")
-
-    def create_table_if_not_exists(db_name, table_name):
-        db = db_name + ".db"
-        conn = sqlite3.connect(db)
-
-        cursor = conn.cursor()
-        cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'")
-        table_exists = cursor.fetchone()
-
-        if not table_exists:
-            cursor.execute(f"""
-            CREATE TABLE {table_name} (
-                id INTEGER PRIMARY KEY,
-                device_id INTEGER,
-                date DATE,
-                kwh REAL
-            );
-            """)
-            conn.commit()
-            print(f"Table {table_name} created.")
-        else:
-            print(f"Table {table_name} already exists.")
-
-        conn.close()
+        # daily_kwh = sum([record[3] for record in hourly_kwh_records])
+        # print(f"Daily kWh stored for {start_date.date()} - {end_date.date()}: {daily_kwh}")
